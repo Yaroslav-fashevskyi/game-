cc2e29ff7d6d7f37fdeb9cca484d72fd35662da2
Yaroslav <117809963+Yaroslav-fashevskyi@users.noreply.github.com>
2022-12-21 08:39:26 +0200

    Доробив гру

---
+++ Game-1-main.py
@@ -0,0 +1,86 @@
+#from Hero import Hero
+
+#hero1 = Hero("Patrick star", 50, 1, #5, "Brain storm")
+#hero2 = Hero("Sponge Bob", 50, 5, #10, 'Soap buble')
+
+#hero1.print_info()
+#hero2.print_info()
+
+#hero1.strike(hero2)
+
+from Hero import Hero
+
+import time
+
+hero1 = Hero("Бот0", 6, 80, 1, "Бот0")
+hero2 = Hero("Бот1", 90, 1, 2, "Бот1")
+hero3 = Hero("Бот2", 40, 7, 9, "Бот2")
+
+
+hero1.print_info()
+time.sleep(2)
+
+hero2.print_info()
+time.sleep(2)
+
+hero3.print_info()
+time.sleep(2)
+
+print("З'єднання з сервером...")
+time.sleep(2)
+
+print("#---------10%")
+time.sleep(5)
+print("##--------20%")
+time.sleep(5)
+print("###-------30%")
+time.sleep(5)
+print("####------40%")
+time.sleep(4)
+print("#####-----50%")
+time.sleep(1)
+print("######----60%")
+time.sleep(3)
+print("#######---70%")
+time.sleep(10)
+print("########--80%")
+time.sleep(5)
+print("#########-90%")
+time.sleep(4)
+print("##########100%")
+time.sleep(2)
+
+
+
+
+print("Привіт бот1")
+time.sleep(2)
+print(
+  "привіт бот0")
+time.sleep(2)
+
+print("Помилка підключення. Зупинка гри .......")
+time.sleep(5)
+
+print("Підключення відновлено гра продовжується")
+
+print("бот1 розібрав бота0")
+time.sleep(5)
+
+print("бот0: RIP")
+time.sleep(5)
+
+print("Бот 1 пішов шукати бота2")
+time.sleep(5)
+
+print("бот1 в .......")
+time.sleep(1)
+
+print("помилка з'єднання з сервером")
+time.sleep(5)
+
+print("Error 404")
+time.sleep(5)
+
+
+
